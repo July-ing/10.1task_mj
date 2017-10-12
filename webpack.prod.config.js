737dc2a4eda8b62f26794945942accd51a3c8946
July_ing <July_ing279@163.com>
2017-10-12 15:17:54 +0800

    modify css loader: delete style-loader

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -23,7 +23,7 @@ module.exports = {
         loaders: [
             { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader!less-loader') },
-            { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader') },
+            { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract( 'css-loader!postcss-loader') },
             { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
             { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
         ]
